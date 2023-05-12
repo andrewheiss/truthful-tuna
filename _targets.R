2ad65a7a069b7bd7bdb6c3addbcb4a3954acda94
Andrew Heiss <andrew@andrewheiss.com>
2023-05-11 21:52:15 -0400

    Rename target for consistency

--- _targets.R
+++ _targets.R
@@ -71,7 +71,7 @@ list(
   ## Analysis ----
   tar_target(summary_activities, make_activities_summary(survey_orgs)),
   tar_target(models_activities, make_activities_models(summary_activities)),
-  tar_target(strategies_size_models, make_strategies_size_models(survey_all)),
+  tar_target(models_strategies_size, make_strategies_size_models(survey_all)),
     
   ## Manuscript and analysis notebook ----
   tar_quarto(output_nice, path = "manuscript", quiet = FALSE, profile = "nice"),
